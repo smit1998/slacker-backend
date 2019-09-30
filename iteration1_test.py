8c4c13c13546d6388e3520e4938a927133e985fc
Andy Wei <email@example.com>
2019-09-30 21:58:55 +1000

    auth_login and auth_logout

--- iteration1_test.py
+++ iteration1_test.py
@@ -1,76 +1,66 @@
 import pytest 
 import re
 
-class token:
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password 
+regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'     
 
-# What's the functionality of regex
-regex = 'sfjkngklsmbv@sf'   
-        
 def auth_login(email, password):
     
+    token = {}
     if(re.search(regex, email)):
-        print('valid email')
-    else:
-        raise Exception('Invalid email')
-        
-    if (len(password) < 5):
-        raise Exception('Invalid password')
-    else:
-        p1 = token(email, password)
-    return p1
+        try:
+            token["email"] = email
+        except Exception:
+            print("Got wrong e-mail")
+    if(len(password) > 5):
+        try:
+            token["password"] = password
+        except Exception:
+            print("Got wrong password")
+    return token
     
-    
-<<<<<<< HEAD
+
 def auth_logout(token):
-    if (token.email == 
-
-
-def auth_register(email, password, name_first, name_last):
     
-
-kjew
-
-
-
-
+    if(re.search(regex, token["email"]) and len(token["password"]) > 5):
+        del(token["email"])
+        del(token["password"])
+        return token 
+    return token 
+    
+   
+# when both of email and password are valid
 def test_1():
     result = auth_login('ankitrai326@gmail.com', '224232r4')
-    assert result.email == 'ankitrai326@gmail.com'
-    assert result.password == '224232r4'
+    assert result == {"email": 'ankitrai326@gmail.com', "password": '224232r4'}
+    
+    result = {"email": 'ankitrai326@gmail.com', "password": '224232r4'}
     auth_logout(result)
+    assert result == {}
 
-'''def test_2():
-    result = auth_login('219900Andy.Wei', 'er22r2222')
-    assert result.email == 'Invalid email'
-    assert result.password == 'er22r2222'
-'''
-
-'''def test_3():
-    result = auth_login('200023Andy@.com', '1234')
-    assert result.email == '200023Andy@.com'
-    assert result.password == 'Invalid password'
-
-=======
-def test_1(): #this tests if the code works 
-    result = auth_login('ankitrai326@gmail.com', '224232r4')
-    assert result.email == 'ankitrai326@gmail.com'
-    assert result.password == '224232r4'
-    pass 
+# when the email is valid and password is invalid
+def test_2(): 
+    result = auth_login('ankitrai326@gmail.com', '2242')
+    assert result == {"email": 'ankitrai326@gmail.com'}
     
-def test_2(): # this tests the password if statement < 5
-    result = auth_login('1337memes@gmail.com', '123') 
-    assert result.email == '1337memes@gmail.com' 
-    #assert result.password == '123' 
-    pass
->>>>>>> d2958806598942a08e68bdf5857258c9fc6c4318
+    result = {"email": 'ankitrai326@gmail.com', "password": '2242'}
+    auth_logout(result)
+    assert result == {"email": 'ankitrai326@gmail.com', "password": '2242'}
     
-def test_3(): #this tests the email if statement (vaild) 
-    result = auth_login('thisisatest.com@gamil', '54351222')
-    #assert result.email == 'thisisatest.com@gamil' 
-    assert result.password == '54351222'
-    pass 
-
+# when the password is valid and email is invalid
+def test_3(): 
+    result = auth_login('1337memesgmail.com', '123243223') 
+    assert result == {'password': '123243223'}
+    
+    result = {'email': '1337memesgmail.com', 'password': '123243223'}
+    auth_logout(result)
+    assert result == {'email': '1337memesgmail.com', 'password': '123243223'}
+    
+# when both of email and password are invalid
+def test_4(): 
+    result = auth_login('tisisatest.comgamil', '66666')
+    assert result == {}   
+    
+    result = {'email': 'tisisatest.comgamil', 'password': '66666'}
+    auth_logout(result)
+    assert result == {'email': 'tisisatest.comgamil', 'password': '66666'}
 
