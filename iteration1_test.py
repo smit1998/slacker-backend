2768dec03290d402c27971b232b3038bc0932f6f
Smit Dobaria <dragondobaria1998@gmail.com>
2019-10-05 12:35:47 +1000

    user_setname

--- iteration1_test.py
+++ iteration1_test.py
@@ -1,6 +1,8 @@
 import pytest 
 import re
+
 #user first and last names are less than 50
+
 def user_profile_setname_test_1():
     user_setname = auth_register('abcd@gmail.com','idontkonwpass','smit','dobaria')
     token = user_setname['token']
