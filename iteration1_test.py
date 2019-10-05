334a0075668b741299d3a7514dad83a5afa57c50
Smit Dobaria <dragondobaria1998@gmail.com>
2019-10-05 12:29:49 +1000

    user_setname

--- iteration1_test.py
+++ iteration1_test.py
@@ -1,74 +1,18 @@
 import pytest 
 import re
+#user first and last names are less than 50
+def user_profile_setname_test_1():
+    user_setname = auth_register('abcd@gmail.com','idontkonwpass','smit','dobaria')
+    token = user_setname['token']
+# updating user first name and last name
+    user_profile_setname('abcd@gmail.com','idontknowpass','cameron','burrell')
+    assert check_names('abcd@gmail.com','idnotknowpass') == ('cameron','burrell')
 
-regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'     
+#user first name is more than 50 words and last name is less than 50
 
-def auth_login(email, password):
-    
-    token = {}
-    if(re.search(regex, email)):
-        try:
-            token["email"] = email
-        except Exception:
-            print("Got wrong e-mail")
-    if(len(password) > 5):
-        try:
-            token["password"] = password
-        except Exception:
-            print("Got wrong password")
-    return token
-    
+def user_profile_setname_test_2():
+    user_setname = auth_register('abcd@gmail.com','idontkonwpass','ansjdhaahahahahahahahahahahabdvdsvsgsdgsdgsdbxchgsdbseghdsbsdgsdbdsgsdbsdsd','dobaria')
+    token = user_setname['token']
 
-def auth_logout(token):
-    
-    if(re.search(regex, token["email"]) and len(token["password"]) > 5):
-        del(token["email"])
-        del(token["password"])
-        return token 
-    return token 
-    
-   
-# when both of email and password are valid
-def test_1():
-    result = auth_login('ankitrai326@gmail.com', '224232r4')
-    assert result == {"email": 'ankitrai326@gmail.com', "password": '224232r4'}
-    
-    result = {"email": 'ankitrai326@gmail.com', "password": '224232r4'}
-    auth_logout(result)
-    assert result == {}
-
-# when the email is valid and password is invalid
-def test_2(): 
-    result = auth_login('ankitrai326@gmail.com', '2242')
-    assert result == {"email": 'ankitrai326@gmail.com'}
-    
-    result = {"email": 'ankitrai326@gmail.com', "password": '2242'}
-    auth_logout(result)
-    assert result == {"email": 'ankitrai326@gmail.com', "password": '2242'}
-    
-# when the password is valid and email is invalid
-def test_3(): 
-    result = auth_login('1337memesgmail.com', '123243223') 
-    assert result == {'password': '123243223'}
-    
-    result = {'email': '1337memesgmail.com', 'password': '123243223'}
-    auth_logout(result)
-    assert result == {'email': '1337memesgmail.com', 'password': '123243223'}
-    
-# when both of email and password are invalid
-def test_4(): 
-    result = auth_login('tisisatest.comgamil', '66666')
-    assert result == {}   
-    
-    result = {'email': 'tisisatest.comgamil', 'password': '66666'}
-    auth_logout(result)
-    assert result == {'email': 'tisisatest.comgamil', 'password': '66666'}
-
-##########################################################################################
-##########################################################################################
-
-def user_profile_setname(token, name_first, name_last):
-    return 
-
-def user_profile_setname_TEST():
-    assert user_profile_setname()
+    with pytest.raises(ValueError):
+        
