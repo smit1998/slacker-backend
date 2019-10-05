f60cbcab24d234ff8ae606d8336edcfbc133e86c
Smit Dobaria <dragondobaria1998@gmail.com>
2019-10-05 13:07:05 +1000

    user_setname

--- iteration1_test.py
+++ iteration1_test.py
@@ -13,8 +13,23 @@ def user_profile_setname_test_1():
 #user first name is more than 50 words and last name is less than 50
 
 def user_profile_setname_test_2():
-    user_setname = auth_register('abcd@gmail.com','idontkonwpass','ansjdhaahahahahahahahahahahabdvdsvsgsdgsdgsdbxchgsdbseghdsbsdgsdbdsgsdbsdsd','dobaria')
+    user_setname = auth_register('abc@gmail.com','abcd123','ansjdhaahahahahahahahahahahabdvdsvsgsdgsdgsdbxchgsdbseghdsbsdgsdbdsgsdbsdsd','dobaria')
     token = user_setname['token']
 
     with pytest.raises(ValueError):
-        
+        user_profile_setname('token','ansjdhaahahahahahahahahahahabdvdsvsgsdgsdgsdbxchgsdbseghdsbsdgsdbdsgsdbsdsd','dobaria')
+
+def user_profile_setname_test_3():
+    user_setname = auth_register('ab@gmail.com','ab456','mike','nasdfkbjdakjdfakjlbfalsbfbasfbalsfjlasbfbjsadflafdsafsfasdfasfaafa')
+    token = user_setname['token']
+
+    with pytest.raises(ValueError):
+        user_profile_setname('token','mike','nasdfkbjdakjdfakjlbfalsbfbasfbalsfjlasbfbjsadflafdsafsfasdfasfaafa')
+
+#both are invalid
+def user_profile_setname_test_4():
+    user_setname = auth_register('abc@gmail.com','abcd123','askjjbvdansjdhaahahahahahahahahahahabdvdsvsgsdgsdgsdbxchgsdbseghdsbsdgsdbdsgsdbsdsd','saljdchalsvchvakchvaskcvkasvcksvckvckavkcvadskhcvakhscvkhavckhgackavkcacvasvgc')
+    token = user_setname['token']
+
+    with pytest.raises(ValueError):
+        user_profile_setname('token','askjjbvdansjdhaahahahahahahahahahahabdvdsvsgsdgsdgsdbxchgsdbseghdsbsdgsdbdsgsdbsdsd','saljdchalsvchvakchvaskcvkasvcksvckvckavkcvadskhcvakhscvkhavckhgackavkcacvasvgc')
