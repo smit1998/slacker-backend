78432f600240415d5b2fb67c5b218367bb96c9ef
Andy Wei <email@example.com>
2019-10-24 20:56:13 +1100

    the server.py(auth.py)latest version

--- server/auth.py
+++ server/auth.py
@@ -2,20 +2,30 @@ from json import dumps
 from flask import Flask, request
 import re
 import jwt 
+from werkzeug.exceptions import HTTPException
+
 import hashlib
-import datatime 
+import dateutil 
+from flask_cors import CORS
 
 APP = Flask(__name__)
 
-# the difference between SECRET and secret  (on video 15.00 mins)
 SECRET = 'comp1531 project'
  
 data = {
-    'user_info': [{}],
-    'channel_info': [{}],
-    'message_info': [{}]
+    'user_info': [],
+    'channel_info': [],
+    'message_info': []
 }
 
+user_id = 0
+
+def generateU_id(u_id):
+    global user_id
+    user_id = user_id + 1
+    u_id = user_id
+    return u_id 
+    
 def getData():
     global data
     return data
@@ -23,17 +33,32 @@ def getData():
 def sendSuccess(data):
     return dumps(data)
 
-# do we need that ? ? ? ?
-def sendError(message):
-    return dumps({
-        '_error' : message,
+def defaultHandler(err):
+    response = err.get_response()
+    response.data = dumps({
+        "code": err.code,
+        "name": "System Error",
+        "message": err.debadscription,
     })
+    response.content_type = 'application/json'
+    return response
 
+app = Flask(__name__)
+app.config['TRAP_HTTP_EXCEPTIONS'] = True
+app.register_error_handler(Exception, defaultHandler)
+CORS(app)
+
+class ValueError(HTTPException):
+    code = 400
+    message = 'No message specified'
+
+# transform this token from bytes into string
 def generateToken(name_first):
     global SECRET
-    encoded = jwt.encode({'name_first': Andy}, SECRET, algorithm='HS256')
-    return str(encoded)
+    encoded = jwt.encode({'name_first': name_first}, SECRET, algorithm='HS256')
+    return encoded.decode('utf-8') # asdfasdfadsf
 
+# return a dictionary 
 def getUserFromToken(token):
     global SECRET
     decoded = jwt.decode(token, SECRET, algorithms=['HS256'])
@@ -47,9 +72,6 @@ def findUser(inputName):
         if u['name_first'] == inputName:
             return u
     return None
-
-def generateU_id(name_last):
-    return name_last + 'z5183885'
 
 timer = None 
 
@@ -68,53 +90,54 @@ def user_register():
     data = getData()
     email = request.form.get('email')
     if(not re.search(regex, email)):
-        raise ValueError("invalid email")
+        raise ValueError(description = "invalid email")
     password = request.form.get('password')
     if(len(password) < 6):
-        raise ValueError("invalid password")
+        raise ValueError(description = "invalid password")
     name_first = request.form.get('name_first')
     if(len(name_first) < 1 or len(name_first) > 50):
-        raise ValueError("invalid name_first")
+        raise ValueError(description = "invalid name_first")
     name_last = request.form.get('name_last')
-    if(len(name_last) < 1 or len(name_first) > 50):
-        raise ValueError("invalid name_last")
+    if(len(name_last) < 1 or len(name_last) > 50):
+        raise ValueError(description = "invalid name_last")
     data['user_info'].append({
         'email': email,
         'password': hashPassword(password),
         'name_first': name_first,
-        'name_last': name_last
-        # HOW TO RESET THE FLAG EFFECTIVELY ! ! ! ? ? ?
-        'flag' : False
+        'name_last': name_last,
+        'u_id': generateU_id(user_id)
     })
     return sendSuccess({
         'token': generateToken(name_first),
-        'u_id': generateU_id(name_last)
+        'u_id': data['user_info'][-1]['u_id']
     })
 
-
+# how to test email doesn't belong to user 
 @APP.route('/auth/login', methods=['POST'])
 def user_login():
     data = getData()
     email = request.form.get('email')
-    password = request.form.get('password')
-    for user in data['user_info']:
-        if(email == user['email'] and password == hashPassword(user['password'])): 
-            newToken = generateToken(user['name_first'])
-            user['token'].append(newToken)
-            return sendSuccess({
-                'token': newToken,
-                'u_id': generateU_id(email)
-            })
-    raise ValueError("Can't login in because of error message")
+    password = hashPassword(request.form.get('password'))
+    print(password)
+    if(email == data['user_info'][-1]['email'] and hashPassword(password) == hashPassword(data['user_info'][-1]['password'])): 
+        newToken = generateToken(data['user_info'][-1]['name_first'])
+        data['user_info'][-1]['token'] = newToken
+        print(data['user_info'][-1])
+        return sendSuccess({
+            'token': newToken,
+            'u_id': data['user_info'][-1]['u_id']
+        })
+    else:
+        raise ValueError(description = "Can't login in because of error message")
 
 
 @APP.route('/auth/logout', methods=['POST'])
 def user_logout():
     data = getData()
-    inputToken = request.form.get('token')
+    inputToken = generateToken(request.form.get('name_first'))
     user = getUserFromToken(inputToken)
-    if user is not None:
-        user['token'].remove(inputToken)
+    if user['name_first'] is not None:
+        del user['token']
         return sendSuccess({
             'is_success': True
         })
@@ -122,43 +145,39 @@ def user_logout():
         'is_success': False
     })
     
-# HOW TO CHECK THE ACCESSERROR ? ? ? ? ? 
 @APP.route('/channel/invite', methods=['POST'])
 def channel_invite():
     data = getData()
-    token = request.form.get('token')
+    token = generateToken(request.form.get('name_first'))
     channel_id = request.form.get('channel_id')
-    u_id = request.form.get('u_id')
+    u_id = generateU_id(request.form.get('u_id'))
     # consider the one AccessError
+    flag_1 = False
     for channel in data['channel_info']:
         for i in channel['all_members']:
-            if(getUserFromToken(token) == i):
-                channel['flag'] = True
-    if(channel['flag'] == False):
-        raise AccessError('this authorised user is not in this channel')
-    # reset the flag
-    channel['flag'] = False
+            user = getUserFromToken(token)
+            if(user['u_id'] == i):
+                flag = True
+    if(flag_1 == False):
+        raise AccessError(discription = 'this authorised user is not in this channel')
+    flag_2 = False
     # consider the u_id ValueError
     for user in data['user_info']:
         if(u_id == user['u_id']):
-            user['flag'] = True
-    if(user['flag'] == False):
+            flag_2 = True
+    if(flag_2 == False):
         raise ValueError("u_id is invalid")
-    # reset the flag
-    user['flag'] = False
+    flag_3 = False
     # consider the channel_id ValueError
     for channel in data['channel_info']:
         if(channel_id == channel['name']):
             for i in channel['all_members']:
-                if(getUserFromToken(token) == i):
-                    channel['flag'] = True
-    if(channel['flag'] == False):
+                user = getUserFromToken(token)
+                if(user['u_id'] == i):
+                    flag_3 = True
+    if(flag_3 == False):
         raise ValueError("channel_id is invalid")
-    # reset the flag
-    channel['flag'] = False
-    # append a new user inside the channel
-    # How do we locate this channel ? ? ? ? I just assume the channel_id is name of this channel.
-    # is that right ?
+    # invite this user into this channel
     for i in data['channel_id']:
         if(channel_id == i['name']):
             i['all_members'].append(u_id)
@@ -168,7 +187,7 @@ def channel_invite():
 @APP.route('/channel/details', methods=['GET'])
 def channel_details():
     data = getData()
-    inputToken = request.args.get('token')
+    inputToken = generateToken(request.args.get('token'))
     channel_id = request.args.get('channel_id')
     flag_1 = False
     flag_2 = False
@@ -198,19 +217,21 @@ def channel_details():
 @APP.route('/user/profile', methods=['GET'])
 def user_profile_server():
     data = getData()
-    token = request.args.get('token')
+    token = generateToken(request.args.get('token'))
     u_id = request.args.get('u_id')
     flag = False
     user_display = {}
+    print(data['user_info'])
     for i in data['user_info']:
-        if(u_id == i['u_id']):
+
+        if(u_id == i.get('u_id')):
             flag = True
             user_display['email'].append(i['email'])
             user_display['name_first'].append(i['name_first'])
             user_display['name_last'].append(i['name_last'])
             user_display['handle_str'].append('win win win')
-    if(flag == False);
-        raise ValueError("u_id is invalid")
+    if(flag == False):
+        raise ValueError(description = "u_id is invalid")
     return sendSuccess({
         'email': user_display['email'],
         'name_first': user_display['name_first'],
@@ -243,7 +264,7 @@ def channel_messages_server():
     if(flag_2 == False):
         raise AccessError('u_id is not in this channel')
     if(start > len(current_channel['messages'])):
-        raise ValueError=('start is greater than the total number of messages')
+        raise ValueError('start is greater than the total number of messages')
     return sendSuccess({
         'messages': current_channel['messages'],
         'start': start,
@@ -274,7 +295,7 @@ def send_message_server():
         'message_id': return_message_id
     })
     
-@APP.route('message/sendlater', methods=['POST'])
+@APP.route('/message/sendlater', methods=['POST'])
 def sendlater_message_server():
     data = getData()
     inputToken = request.form.get('token')
@@ -307,6 +328,6 @@ def sendlater_message_server():
     
     
 if __name__ == '__main__':
-    APP.run()
+    APP.run(debug = True, port=5051)
 
 
