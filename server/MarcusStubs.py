16af0f0c6c2d942c8ce7992263b46093135b393c
Marcus <18moyang@gmail.com>
2019-10-25 16:15:29 +1100

    Channels Create half

--- server/MarcusStubs.py
+++ server/MarcusStubs.py
@@ -1,10 +1,44 @@
+from json import dumps
+from flask import Flask
+
+app = Flask(__name__) 
+
+SECRET = 'comp1531 project'
+
+data = {
+    'user_info': [],
+    'channel_info': [],
+    'message_info': []
+}
+
+channel_id = 0
+
+def generateChannel_id():
+    global channel_id
+    channel_id += 1
+    return channel_id
+
+def generateToken(name_first):
+    global SECRET
+    encoded = jwt.encode({'name_first': name_first}, SECRET, algorithm='HS256')
+    return encoded.decode('utf-8')
+
+def getData():
+    global data
+    return data
+
 def channels_list(token):
     return channels
 
 def channels_listall(token):
     return channels
 
+@app.route('/channel/create', methods=['POST'])
 def channels_create(token, name, is_public):
+    data = getData()
+    inputToken = generateToken(request.args.get('token'))
+    channel_id = generateChannel_id()
+
     return channel_id
 
 def message_remove(token, message_id):
