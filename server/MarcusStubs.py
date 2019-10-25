20100e88a34f8786b7ec0cc13b191bc683e5b8b5
Marcus <18moyang@gmail.com>
2019-10-26 02:17:10 +1100

    2 out of 8

--- server/MarcusStubs.py
+++ server/MarcusStubs.py
@@ -7,20 +7,21 @@ SECRET = 'comp1531 project'
 
 data = {
     'user_info': [],
-    'channel_info': [],
+    'channels': [],
     'message_info': []
 }
 
-channel_id = 0
+ch_id = 0
 
 def generateChannel_id():
-    global channel_id
-    channel_id += 1
+    global ch_id
+    ch_id += 1
+    channel_id = channel_id
     return channel_id
 
-def generateToken(name_first):
+def generateToken(u_id):
     global SECRET
-    encoded = jwt.encode({'name_first': name_first}, SECRET, algorithm='HS256')
+    encoded = jwt.encode({'u_id': u_id}, SECRET, algorithm='HS256')
     return encoded.decode('utf-8')
 
 def getData():
@@ -31,30 +32,33 @@ def sendSuccess(data):
     return dumps(data)
 
 @app.route('/channel/list', methods=['GET'])
-def channels_list(token):
+def channels_list():
     return
 
 @app.route('/channel/listall', methods=['GET'])
-def channels_listall(token):
-    return
+def channels_listall():
+    list_all_channels = {}
+    for channel in data['channels']:
+        list_all_channels.append(channel['channel_id'])
+    return sendSuccess(allChannels)
 
 @app.route('/channel/create', methods=['POST'])
 def channels_create():
     data = getData()
-    inputToken = generateToken(request.args.get('token'))
-    channelName = request.args.get('name')
-    if (len(channelName) > 20):
+    input_token = generateToken(request.args.get('token'))
+    channel_name = request.args.get('name')
+    if (len(channel_name) > 20):
         raise ValueError(description = "invalid channel name")
     is_public = request.args.get('is_public')
-    data['channel_info'].append({
+    data['channels'].append({
         'channel_id': generateChannel_id(),
-        'owner_members': inputToken,
-        'all_members': inputToken,
-        'name': channelName,
+        'owner_members': input_token,
+        'all_members': input_token,
+        'name': channel_name,
         'is_public': True
     })
     return sendSuccess({
-        'channel_id': data['channel_info'][-1]['channel_id']
+        'channel_id': data['channels'][-1]['channel_id']
     })
 
 def message_remove(token, message_id):
