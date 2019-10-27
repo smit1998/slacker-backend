342b2ad8a087f39d9b433400d1bde9d99811557f
Cameron <cameron.ha@hotmail.com>
2019-10-27 18:39:31 +1100

    broken just like trimesters

--- iteration2_cameron.py
+++ iteration2_cameron.py
@@ -1,16 +1,20 @@
-json import dumps 
-from flask import Flask, request 
-from .Classes
-import re 
-import jwt
+from json import dumps
+from flask import Flask, request
+from flask_mail import Mail, Message
+import re
+#import jwt 
 from werkzeug.exceptions import HTTPException
-
-#from .classes import Users
-#from .classes import Channels
-
-import hashlib 
+import copy 
+import hashlib
 import dateutil 
 from flask_cors import CORS
+#from dummy_error import AccessError
+from datetime import timezone
+import random 
+import string 
+
+
+
 
 SECRET = 'comp1531 project' 
 
@@ -59,8 +63,16 @@ APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 APP.register_error_handler(Exception, defaultHandler)
 CORS(APP)
 
+APP.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME = 'HASCdevteam@gmail.com',
+    MAIL_PASSWORD = "Alltheboys"
+)
+
 @APP.route('/channel/leave', methods=['POST']) 
-    def channel_leave(): 
+def channel_leave(): 
     data = getData()
     token = generateToken(request.form.get('token'))
     channelID = int(request.form.get('channel_id'))
@@ -79,7 +91,7 @@ CORS(APP)
     if(flag_1 == False):
         raise ValueError(description = 'channel_id is invalid')
     return sendSuccess({})
-    
+''' 
 @APP.route('/channel/join', methods=['POST'])
     def channel_join():
     data = getData()
@@ -94,11 +106,13 @@ CORS(APP)
             if channel['is_public'] == True: 
                 channel['all_members'].append(user)
             else: 
-            '''
+            
+           
                 for i in channel['owners']: 
                     if (user == channel[i]):
-                        channel['all_members'].append(user)
-           ''' #need channel permissions 
+                     channel['all_members'].append(user)
+            
+           #need channel permissions 
     if (flag_1 == False): 
         raise ValueError('channel_id is invalid')
     if (flag_2 == False): 
@@ -126,7 +140,7 @@ def addowner(tken):
                 if (flag_2 == False and flag_3 == True): 
                     channel['owners'].append(user_id)
     if(flag_1 == False): 
-        raise ValueError('channel id is not a valid channel) 
+        raise ValueError('channel id is not a valid channel') 
     if(flag_2 == True): 
         raise ValueError('the user id you are making an owner is already and owner')
     if (flag_3 == False): 
@@ -162,20 +176,37 @@ def removeowner():
         raise AccessError('the authorsied user is not an owner of the channel') 
     # permission ned channel permissions function 
     return sendSuccess({})
-    
-@APP.route('/auth/passwordrest/request', methods=['POST']) # ask the bois need to do this and message edit fix any potential bugs and make sure my code is right 
-
+    '''
+@APP.route('/auth/passwordrest/request', methods=['POST']) 
 def passwordreset_request():
     data = getData() 
-    email = request.form.get('email')
+    mail = Mail(APP)
+    email = 'cameron.ha@hotmail.com'#request.form.get('email')
     flag = False
+    code = int(random_code_generator(10))
     for user in data['user_info']: 
+        user['email'].append(email)
         if user['email'] == email:
             flag = True
+            user['reset_code'] = code #added to the data structure 
+            first_name = user['first_name'] 
+            try:
+                msg = Message("Reset, Password Request Slackr",
+                    sender="HASCdevteam@gmail.com",
+                    recipients=email)
+                msg.body = 'Hi' + first_name + 'You have requested for a change in your password, please use the code provided below to reset your account.\n' + code + '\n regards the slackr development, team.'
+                mail.send(msg)
+               # return 'Mail sent!'
+           # except Exception as e:
+               # return (str(e))
+def random_code_generator(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
-
-
-@APP.route('/auth/passwordreset/reset'). methods=['POST'])
+    
+'''
+@APP.route('/auth/passwordreset/reset', methods=['POST'])
 def passwordreset_reset():
     data = getData()
     reset_code = request.form.get('reset_code')
@@ -206,3 +237,7 @@ def message_edit(token, message_id, message):
         if(message[message_id] == input_message_id): 
             if
             message[message] = input_message
+
+'''
+if __name__ == '__main__':
+    APP.run(debug = True, port=4002)
