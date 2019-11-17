6f70a72999b40ef85c5683c26a478299b76247f6
Andy Wei <email@example.com>
2019-11-17 19:06:34 +1100

    delete

--- server/echo_test.py
+++
@@ -1,8 +0,0 @@
-from echo import echo
-
-def test_echo():
-    assert echo("1") == "1", "1 == 1"
-    assert echo("abc") == "abc", "abc == abc"
-    assert echo("trump") == "trump", "trump == trump"
-
-
